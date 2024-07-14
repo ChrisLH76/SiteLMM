076728c7e006c10767e1cd8422448c73178349ed
ChristopheE <linechris76@gmail.com>
2024-07-14 02:35:56 +0200

    Script NavBar Transition

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const navbar = document.querySelector(".navbar-container");
+const navbar = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 120) {
